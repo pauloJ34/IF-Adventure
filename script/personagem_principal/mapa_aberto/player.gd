b88ab4b9e060b3941117a37830ae411f613292e8
pauloJ34 <paulofranklin612@gmail.com>
2023-08-18 23:25:48 -0300

    atualização do mapa

--- script/personagem_principal/mapa_aberto/player.gd
+++ script/personagem_principal/mapa_aberto/player.gd
@@ -10,35 +10,54 @@ func _physics_process(_delta: float) -> void:
 	move()
 
 func move() -> void:
-
 	if Input.is_action_pressed("ui_left") || Input.is_action_pressed("ui_right"):
-		
 		motion.x = (
 			Input.get_action_strength("ui_right")-
 			Input.get_action_strength("ui_left")) * speed
 		motion.y = 0
+		motion = move_and_slide(motion)
 		
 		if motion.x > 0:
 			animation.play("walk-right")
 			Global.last_player_stand = 0
-		else:
+		elif motion.x < 0:
 			animation.play("walk-left")
 			Global.last_player_stand = 1
+		else: 
+			# quando houver uma colição o personagem não vai poder se mover mais na direção onde tem a colição
+			if Input.is_action_pressed("ui_left"):
+				animation.play("idle-left")
+				Global.last_player_stand = 1
+			else: 
+				animation.play("idle-right")
+				Global.last_player_stand = 0
 		
 	elif Input.is_action_pressed("ui_up") || Input.is_action_pressed("ui_down"):
 		motion.x = 0
 		motion.y = (
 			Input.get_action_strength("ui_down")-
 			Input.get_action_strength("ui_up")) * speed
+		motion = move_and_slide(motion)
 		
 		if motion.y > 0:
 			animation.play("walk-bottom")
 			Global.last_player_stand = 2
-		else:
+		elif motion.y < 0:
 			animation.play("walk-top")
 			Global.last_player_stand = 3
+		else:  
+			#quando houver uma colição o personagem não vai poder se mover mais na direção onde tem a colição
+			if Input.is_action_pressed("ui_down"):
+				animation.play("idle-bottom")
+				Global.last_player_stand = 2
+			else: 
+				animation.play("idle-top")
+				Global.last_player_stand = 3
 			
 	else:
+		motion.x = 0
+		motion.y = 0
+		motion = move_and_slide(motion)
 		
 		if Global.last_player_stand == 1:
 			animation.play("idle-left")
@@ -48,9 +67,6 @@ func move() -> void:
 			animation.play("idle-bottom")
 		elif Global.last_player_stand == 3:
 			animation.play("idle-top")
-		
-		motion.x = 0
-		motion.y = 0
 	
-	motion = move_and_slide(motion)
+	
 	
